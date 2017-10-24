232210e531e5c2a752ac4ba6da0b97c5ed1b3962
MalteBartels <malte.bartels@live.de>
2017-10-24 10:47:53 +0200

    API connection in Main's props

--- src/main/web/app/app.js
+++ src/main/web/app/app.js
@@ -44,5 +44,5 @@ injectTapEventPlugin();
 // Render the main app react component into the app div.
 // For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
 render(
-  <Main />,
+  <Main getAppointments={getAppointments} getYearlyCalendar={getYearlyCalendar} />,
 document.getElementById('app'));
