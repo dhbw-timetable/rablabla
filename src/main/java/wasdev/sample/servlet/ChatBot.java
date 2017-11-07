79a27870a9ad3d0aa048a15535b9543339fa66f2
Dominik Lenz <mininik88@googlemail.com>
2017-11-07 11:58:29 +0100

    Full Chatbot Implementation
    Added missing functions to chatbot with entity handling and
    date-dependend answers (awesome :smiling_imp: )

--- src/main/java/wasdev/sample/servlet/ChatBot.java
+++ src/main/java/wasdev/sample/servlet/ChatBot.java
@@ -1,6 +1,11 @@
 package wasdev.sample.servlet;
 
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -14,6 +19,11 @@ import com.ibm.watson.developer_cloud.conversation.v1.model.InputData;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
 
+import dhbw.timetable.rapla.data.event.Appointment;
+import dhbw.timetable.rapla.date.DateUtilities;
+import dhbw.timetable.rapla.exceptions.NoConnectionException;
+import dhbw.timetable.rapla.parser.DataImporter;
+
 
 @WebServlet("/ChatBot")
 @MultipartConfig
@@ -24,7 +34,6 @@ public class ChatBot extends HttpServlet {
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String key = req.getParameter("key");
 		String messageToWatson = req.getParameter("text");
 		Conversation service = new Conversation(Conversation.VERSION_DATE_2017_05_26);
 		service.setUsernameAndPassword("f1e1a1c3-da01-4592-a92c-d63003148de2", "c1yTAF028iUH");
@@ -43,6 +52,43 @@ public class ChatBot extends HttpServlet {
 		}
 		response = service.message(options).execute();
 		String answer = response.getOutput().getText().get(0);
+		if(response.getIntents().get(0).getIntent().equals("#startingTime")) {
+			String date = response.getEntities().get(0).getValue();
+			final String url = URLDecoder.decode(req.getParameter("url").replace("+", "%2B"), "UTF-8").replace("%2B", "+");
+			LocalDate searchedDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));			
+			LocalDate week = DateUtilities.Normalize(searchedDate);			
+			// Load data
+			Map<LocalDate, ArrayList<Appointment>> data;
+			try {
+				data = DataImporter.ImportWeekRange(week, week, url);			
+			Appointment first = null;			
+			for (Appointment a : data.get(week))
+				if (a.getStartDate().toLocalDate().equals(searchedDate) && (first == null || a.getStartDate().isBefore(first.getStartDate())))
+					first = a;
+			answer = answer.replace("locicPart", first.getStartTime());
+			} catch (IllegalAccessException | NoConnectionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else if(response.getIntents().get(0).getIntent().equals("#timetable")) {
+			String date = response.getEntities().get(0).getValue();
+			final String url = URLDecoder.decode(req.getParameter("url").replace("+", "%2B"), "UTF-8").replace("%2B", "+");
+			LocalDate searchedDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));			
+			LocalDate week = DateUtilities.Normalize(searchedDate);			
+			// Load data
+			Map<LocalDate, ArrayList<Appointment>> data;
+			try {
+				data = DataImporter.ImportWeekRange(week, week, url);
+				String lessons = "";
+			for (Appointment a : data.get(week))
+				if (a.getStartDate().toLocalDate().equals(searchedDate))
+					lessons = lessons + " " + a.getTitle();
+			answer = answer.replace("locicPart", lessons.trim());
+			} catch (IllegalAccessException | NoConnectionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		resp.getWriter().println(answer);
 	}
 	
