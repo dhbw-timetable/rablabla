206db6e233bf84b344deea5a5b69b76addc30713
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-03-07 10:44:04 +0100

    Fix indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
     "extends": "airbnb",
-	  "parser": "babel-eslint",
+    "parser": "babel-eslint",
     "env": {
       "browser": true
     },
-  	"rules": {
+    "rules": {
       "one-var-declaration-per-line": 0,
       "one-var": 0,
       "radix": 0,
-  		"react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-  		"no-console": ["error", { "allow": ["log", "warn", "error"] }],
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+      "no-console": ["error", { "allow": ["log", "warn", "error"] }],
       "linebreak-style": ["error", "unix"],
       "no-param-reassign": 0,
       "no-alert": 0,
       "no-mixed-operators": 0,
       "no-plusplus": 0,
-  		"react/jsx-no-bind": 0,
-  		"react/jsx-indent": 0,
+      "react/jsx-no-bind": 0,
+      "react/jsx-indent": 0,
       "react/no-array-index-key": 0,
       "react/forbid-prop-types": 0,
-  		"react/arrow-body-style": 0,
-  		"react/sort-comp": 0,
-  		"class-methods-use-this": 0,
-  		"no-return-assign": 0,
-  		"arrow-body-style": 0,
-  		"jsx-a11y/no-static-element-interactions": 0
-  	}
+      "react/arrow-body-style": 0,
+      "react/sort-comp": 0,
+      "class-methods-use-this": 0,
+      "no-return-assign": 0,
+      "arrow-body-style": 0,
+      "jsx-a11y/no-static-element-interactions": 0
+    }
 };
