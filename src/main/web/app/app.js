e8c2204373a1233fbb1528f2940c1eddb037ca84
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-03-12 17:39:33 +0100

    Fix locale to en

--- src/main/web/app/app.js
+++ src/main/web/app/app.js
@@ -1,16 +1,10 @@
 /* global document window */
 import React from 'react';
 import { render } from 'react-dom';
-import moment from 'moment';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import Main from './Main';
 
-require('moment/locale/de');
-
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
-
-moment.locale('de');
-moment.locale();
 
 render(<Main />, document.getElementById('app'));
